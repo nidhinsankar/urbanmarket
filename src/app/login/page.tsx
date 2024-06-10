34c703e19987e84348deb6fe0d30d91fb82be6a3
nidhinsankar <nidhinsankar1@gmail.com>
2024-06-11 00:21:28 +0530

    added shadcn-ui

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,21 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 const Login = () => {
-  return <div>Login page</div>;
+  return (
+    <Card className="max-w-[450px]">
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Label>Email</Label>
+        <Input type="email" />
+        <Label>Password</Label>
+        <Input type="password" />
+      </CardContent>
+    </Card>
+  );
 };
 
 export default Login;
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,5 +1,21 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 const Signup = () => {
-  return <div>Sign</div>;
+  return (
+    <Card className="max-w-[500px]">
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Label>Email</Label>
+        <Input type="email" />
+        <Label>Password</Label>
+        <Input type="password" />
+      </CardContent>
+    </Card>
+  );
 };
 
 export default Signup;
